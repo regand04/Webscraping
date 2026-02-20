efdd89eb2cd0be0ce8a8c94d616d40a826a8b695
regand04 <v25regan@du.se>
2026-02-20 09:26:12 +0100

    Update flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from webscraping import scrape_books
+from book_data import load_books, save_books, add_book, update_book, remove_book
+
+app = Flask(__name__)
+
+if not load_books():
+    save_books(scrape_books())
+
+@app.route("/", methods = ["GET"])
+def home():
+    return "Hello from flask"
+
+@app.route("/api/v1/books", methods = ["GET"])
+def get_books():
+    books = load_books()
+    return jsonify(books), 200
+
+@app.route("/api/v1/books/<int:book_id>", methods = ["GET"])
+def get_book(book_id):
+    books = load_books()
+    for book in books:
+        if book["id"] == book_id:
+            return jsonify(book), 200
+    return jsonify({"error" : "Book Not Found"}), 404
+
+@app.route("/api/v1/books", methods = ["POST"])
+def post_book():
+    new_book = request.json
+
+    #Krav att ha id och titel
+    if "id" not in new_book or "title" not in new_book:
+        return jsonify({"error" : "Book Must Have ID And Title"}), 400
+    if not add_book(new_book):
+        return jsonify({"error" : "BookWith This ID Already Exists."}),  400
+    return jsonify(new_book), 201
+
+@app.route("/api/v1/books/<int:book_id>", methods = ["PUT"])
+def put_book(book_id):
+    updated_data = request.json
+    if update_book(book_id, updated_data):
+        books = load_books()
+        for book  in books:
+            if book["id"] == book_id:
+                return jsonify(book), 200
+    return jsonify({"error" :  "Book Not Found"}), 404
+
+@app.route("/api/v1/books/<int:book_id>", methods = ["DELETE"])
+def delete_book(book_id):
+    if not remove_book(book_id):
+        return jsonify({"error": "Book Not Found"}), 404
+    return jsonify({"message" : f"Book {book_id} deleted"}), 200
+
+if __name__ == "__main__":
+    app.run(debug = True)
